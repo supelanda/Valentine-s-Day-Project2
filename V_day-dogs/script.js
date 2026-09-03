342b978168dfc3dbc788cc4b961005afac5fa31a
supelanda <andrea.pelanda@marconirovereto.it>
2026-09-03 14:18:36 +0300

    Refactor script.js for better readability

--- V_day-dogs/script.js
+++ V_day-dogs/script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "дааа!!";
 
     catImg.src = "Arte.png";
 
